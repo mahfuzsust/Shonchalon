e48fb42f47120ee92670681b508e3870405d8cf3
Md. Mahfuzur Rahman <mahfuz.sust001@gmail.com>
2014-03-08 16:05:31 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $host="localhost"; // Host name 
-$username="user2179168"; // Mysql username 
-$password="Trytobeserious25788752"; // Mysql password 
-$db_name="db2179168-bloodbank"; // Database name 
+$username=""; // Mysql username 
+$password=""; // Mysql password 
+$db_name=""; // Database name 
 
 //Connect to server and select database.
 mysql_connect("$host", "$username", "$password")or die("cannot connect to server"); 
